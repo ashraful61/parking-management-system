75b4a0771094c1e748c372d2fecac09dccf56f80
ashraful61 <ashraful.ru37@gmail.com>
2024-11-17 14:47:18 +0600

    routing updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+    { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
     { path: 'vehicles', loadChildren: () => import('./vehicle/vehicle.module').then(m => m.VehicleModule) },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
